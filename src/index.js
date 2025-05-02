aa28bb8773c404f23bf644179ef77dba46423f7a
juanfra270588 <juan.lopez@rimac.com.pe>
2025-05-02 14:00:06 -0500

    cambio para simular CD

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-const http = require('http');
-const port = 3000;
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>Cambio para probar CI</h1>');
-}).listen(port, () => {
-  console.log(`Servidor escuchando en puerto ${port}`);
-});
+<h1>Versión desplegada</h1>
